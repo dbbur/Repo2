b52a68c04841e5aa7e7a02fe29674caf3c899feb
dbbur <db.burfitt@gmail.com>
2018-04-15 17:24:01 -0600

    Update to Game Objects and Inquiry Classes

--- TestApplicaiton5/GameObject.cs
+++ TestApplicaiton5/GameObject.cs
@@ -2,13 +2,14 @@
  * Doesn't need to be in this class if it makes sense to be elsewhere, just creating a placeholder class.
  * 
  * -Display and confirm if the answer was correct to the user
--Display total source after selected game length is completed.
+-Display total score after selected game length is completed.
 -Randomly generate questions
 
  * 
+ * TODO: (GetInquiryList) Need to create more Inquiry variables for the array.
+ *          (GetInquiryList) Need to only return number of variables in list as are specified in the parameter.
  * 
- * 
- * /
+ */
 
 
 
@@ -19,6 +20,7 @@
 
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,5 +30,18 @@ namespace TestApplicaiton5
 {
     class GameObject
     {
+        /*Takes a int parameter for the number of questions and returns that number of questions in a array List<Inquiry>.  */
+        public List<Inquiry> GetInquiryList(int inqRequested)
+        {
+            //Initilizes a list of Inquiry variables while building the array
+            List<Inquiry> inquiryList = new List<Inquiry>()
+            {
+                new Inquiry {Question = "Which actor stars in the movie Beetleguise?", Answer = "Michael Keaton", MultiChoice1 = "Michael Keaton", MultiChoice2= "Danny DeVito", MultiChoice3 = "David Cross" }
+            };
+
+            return inquiryList;
+        } 
+
+
     }
 }
--- TestApplicaiton5/Inquiry.cs
+++ TestApplicaiton5/Inquiry.cs
@@ -27,5 +27,28 @@ namespace TestApplicaiton5
 {
     class Inquiry
     {
+        //List of properties
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string MultiChoice1 { get; set; }
+        public string MultiChoice2 { get; set; }
+        public string MultiChoice3 { get; set; }
+
+        //Constructor method with no input parameters
+        public Inquiry ()
+        {
+
+        }
+
+        //Constructor method with input parameters
+        public Inquiry (string question, string answer, string multiChoice1, string multiChoice2, string multiChoice3)
+        {
+            this.Question = question;
+            this.Answer = answer;
+            this.MultiChoice1 = multiChoice1;
+            this.MultiChoice2 = multiChoice2;
+            this.MultiChoice3 = multiChoice3;
+        }
+
     }
 }
